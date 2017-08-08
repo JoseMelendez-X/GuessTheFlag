b5b23bd23652f83c29e1ee1135c8dbfd52bb1a13
Jose Melendez <JoseM_Melendez@icloud.com>
2017-08-08 15:58:33 -0400

    Imported Gameplay kit and randomized flags

--- GuessTheFlag/GuessTheFlag/ViewController.swift
+++ GuessTheFlag/GuessTheFlag/ViewController.swift
@@ -6,19 +6,76 @@
 //  Copyright © 2017 JoseMelendez. All rights reserved.
 //
 
+//Frameworks
+
+import GameplayKit
+
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //When the view loads add these countries to the array of countries
-        countries.append("Estonia")
-        countries.append("france")
-        countries.append("germany")
-        countries.append()
         
+        countries.append("estonia")
+        
+        countries.append("france")
+        
+        countries.append("germany")
+        
+        countries.append("ireland")
+        
+        countries.append("russia")
+        
+        countries.append("uk")
+        
+        countries.append("us")
+        
+        countries.append("spain")
+        
+        countries.append("poland")
+        
+        countries.append("italy")
+        
+        countries.append("monaco")
+        
+        countries.append("nigeria")
+        
+        //Or you can do it this way
+        
+       /*
+         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+         */
+        
+        //Border witdth of the buttons
+        button1.layer.borderWidth = 2
+        
+        button2.layer.borderWidth = 2
+        
+        button3.layer.borderWidth = 2
+        
+        //Ask question method
+        askQuestion()
+
+    }
+    
+    //Function "ask question"
+    
+    func askQuestion() {
+        
+        //Randomize the order of the flags
+        
+        countries = GKRandomSource().arrayByShufflingObjects(in: countries) as! [String]
+        
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
         
     }
     
